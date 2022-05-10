3e7b31f13c59fde21662254e385db2049f939071
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-10 09:36:20 +0200

    Estructura creada

---
+++ ej1/src/test/java/com/TEAM4_C4_UD22/ej1/AppTest.java
@@ -0,0 +1,20 @@
+package com.TEAM4_C4_UD22.ej1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
